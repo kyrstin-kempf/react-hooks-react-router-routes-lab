8e10028a8cd5f712cea0ba0564de4b9e68273943
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-09 22:20:27 -0400

    commit tree 52b2c129e6eacd4278a22659c26d12d36822001a

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const moviesList = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {moviesList}
+  </div>
+  );
 }
 
 export default Movies;
